b6f7c0c6f0464de3ceb9345fce39f35f57bc38db
JeromeDaGnome <jeremyitprof@gmail.com>
2022-02-18 18:48:04 +0000

    js work on pattern creation and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+var level = null;
+var patternArray = [];
+
 $("#guest").click(function(){
 
-    let level = 0
+    level = 1
     $("#next").prop("disabled", false).prop("innerText", "Start Round");
     //startGame("guest","none");
 
@@ -8,7 +11,7 @@ $("#guest").click(function(){
 
 $("#user").click(function(){
 
-    let level = 0
+    level = 1
     $("#next").prop("disabled", false).prop("innerText", "Start Round");
     //startGame("John Doe","USA");
     
@@ -16,67 +19,64 @@ $("#user").click(function(){
 
 $("#next").click(function(){
 
-    let mem1 = random(9)
+    //let mem1 = random(9)
+    $("#next").prop("disabled", true).prop("innerText", "Round"+level);
+    setPattern()//creates an array of numbers according to the players level
+    displayPattern()//highlights the numbers in the playfield according to the pattern set
+    //testPattern(patternArray)//validates that each click follows the pattern and continues the game if pattern is matched or ends the game if pattern is not matched
+
+
     
-    setPattern(level)//creates an array of numbers according to the players level
-    displayPattern(patternArray)//highlights the numbers in the playfield according to the pattern set
-    testPattern(patternArray)//validates that each click follows the pattern and continues the game if pattern is matched or ends the game if pattern is not matched
+
+})
 
 
+function setPattern(){
+    //creates an array of numbers according to the players level
+    //returns patternArray variable
+    //console.log(level)
+    patternArray = [];
+    for (var i = 1; i < level+1; i++){
+        let patternInstance = random(9)
+        //console.log(patternInstance)
+        patternArray.push(patternInstance)
+        console.log("Array Set " + patternArray)
+    }
+};
+
+function displayPattern(){
+    //highlights the numbers in the playfield according to the pattern set
+
+    for (var i = 1; i < level+1; i++){
+        let mem1 = patternArray[i-1]
+        console.log(mem1) 
+        $("#btn"+mem1).removeClass("btn-lg btn-outline-primary")
+        $("#btn"+mem1).addClass("btn-lg btn-primary")
+        setTimeout(() => {  $("#btn"+mem1).removeClass("btn-lg btn-primary");
+        $("#btn"+mem1).addClass("btn-lg btn-outline-primary")}, 1000);
+    }
+
+
+};    
+
+function testPattern(){
+    //validates that each click follows the pattern and continues the game if pattern is matched or ends the game if pattern is not matched
     $("[id^=btn]").unbind().click(function(){
         
         if ($(this).attr("id")=="btn"+mem1){
             console.log("Winner Winner Chicken Dinner");
+            level++;
             setTimeout(() => {$("#next").trigger("click")}, 1000);
         } else {
             console.log("You are and always will be a LOSER!");
+            level = 0;
         };
         console.log($(this).attr("id"))
 
     });
 
-})
+};
 
-
-$("#btn1").click(function(){
-    
-})
-$("#btn2").click(function(){
-    
-})
-$("#btn3").click(function(){
-    
-})
-$("#btn4").click(function(){
-    
-})
-$("#btn5").click(function(){
-    
-})
-$("#btn6").click(function(){
-    
-})
-$("#btn7").click(function(){
-    
-})
-$("#btn8").click(function(){
-    
-})
-$("#btn9").click(function(){
-    
-})
-
-function displayPattern(level){
-    //for each loop that runs as many times as the level param
-    array.forEach(element => {
-        
-    });
-    $("#btn"+mem1).removeClass("btn-lg btn-outline-primary")
-    $("#btn"+mem1).addClass("btn-lg btn-primary")
-    setTimeout(() => {  $("#btn"+mem1).removeClass("btn-lg btn-primary");
-    $("#btn"+mem1).addClass("btn-lg btn-outline-primary")}, 2000);
-
-}
 function startGame(username, location){
 
     
